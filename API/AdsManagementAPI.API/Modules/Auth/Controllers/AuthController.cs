0b97829b40afae010c6931d026dffd99539bef08
20127670 <nonameno1f@gmail.com>
2024-09-17 22:28:45 +0700

    feat: adding API - Modules - AuthModule

--- API/AdsManagementAPI.API/Modules/Auth/Controllers/AuthController.cs
+++ API/AdsManagementAPI.API/Modules/Auth/Controllers/AuthController.cs
@@ -1,6 +1,25 @@
-﻿namespace AdsManagementAPI.API.Modules.Auth.Controllers;
+﻿using AdsManagementAPI.Modules.Auth.Application;
+using AdsManagementAPI.Modules.Auth.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
-public class AuthController
+namespace AdsManagementAPI.API.Modules.Auth.Controllers;
+
+[ApiController]
+[Route("api/auth/login")]
+public class AuthController : ControllerBase
 {
-    
+    private readonly IAuthModule _authModule;
+
+    public AuthController(IAuthModule authModule)
+    {
+        _authModule = authModule;
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAuthenticatedUser()
+    {
+        var user = await _authModule.ExecuteQueryAsync(new TestQuery("Hihi"));
+
+        return Ok(user);
+    }
 }
