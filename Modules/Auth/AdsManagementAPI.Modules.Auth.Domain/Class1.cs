e07cff955be3e3b365508d7e325f2134d09f4a31
20127670 <nonameno1f@gmail.com>
2024-09-17 22:13:57 +0700

    feat: adding Modules - Auth - Domain project

---
+++ Modules/Auth/AdsManagementAPI.Modules.Auth.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace AdsManagementAPI.Modules.Auth.Domain;
+
+public class Class1
+{
+}
---
+++ Modules/Auth/AdsManagementAPI.Modules.Auth.Domain/Entities/Officer.cs
@@ -0,0 +1,14 @@
+﻿using AdsManagementAPI.BuildingBlocks.Domain;
+
+namespace AdsManagementAPI.Modules.Auth.Domain.Entities;
+
+public class Officer : Entity
+{
+    public Guid OfficerId { get; set; }
+    public string? FullName { get; set; }
+    public DateTime DoB { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? PasswordHash { get; set; }
+    public int WardId { get; set; }
+}
