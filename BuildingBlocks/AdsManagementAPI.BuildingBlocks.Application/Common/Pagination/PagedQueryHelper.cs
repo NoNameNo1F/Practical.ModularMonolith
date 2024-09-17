25b7714dfa93af3c7662a6c7ae2734d5fdba2d14
20127670 <nonameno1f@gmail.com>
2024-09-17 22:06:21 +0700

    feat: adding BuildingBlock - Application project

---
+++ BuildingBlocks/AdsManagementAPI.BuildingBlocks.Application/Common/Pagination/PagedQueryHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace AdsManagementAPI.BuildingBlocks.Application.Common.Pagination;
+
+public class PagedQueryHelper<T> : List<T>
+{
+    public PageData PageData { get; }
+
+    public PagedQueryHelper(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        PageData = new PageData(count, pageNumber, pageSize);
+        AddRange(items);
+    }
+}
